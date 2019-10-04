812b71e1f02227e5c97067edf2f6b07dc086738c
Tom Hartley <tom@tomhrtly.com>
2019-10-04 21:47:11 +0100

    Add setup

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        'indent': ['error', 4],
+        'max-len': ['error', {
+            'code': 150,
+            'ignoreStrings': true,
+        }],
+        'no-param-reassign': [2, { 'props': false }]
+    },
+};
