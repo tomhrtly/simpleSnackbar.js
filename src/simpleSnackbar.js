fe81224030e08d5a4e09704c42bdc4e13907bee8
Tom Hartley <tom@tomhrtly.com>
2019-10-07 17:48:32 +0100

    Add events method

--- src/simpleSnackbar.js
+++ src/simpleSnackbar.js
@@ -2,7 +2,9 @@ class simpleSnackbar {
     constructor(message, options) {
         this.id = Math.floor(Math.random() * 1000000);
         this.message = message;
+
         this.defaults = {
+            autohide: true,
             icons: {
                 success: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>',
                 info: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info-circle" class="svg-inline--fa fa-info-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>',
@@ -14,6 +16,8 @@ class simpleSnackbar {
         };
         this.options = simpleSnackbar.extend(this.defaults, options);
 
+        this.timer = null;
+
         if (!document.querySelector('.ss-snackbars')) {
             const snackbars = document.createElement('div');
             snackbars.classList.add('ss-snackbars');
@@ -24,6 +28,36 @@ class simpleSnackbar {
         }
 
         this.init();
+    }
+
+    events() {
+        const snackbar = document.querySelector(`.ss-snackbar[data-id="${this.id}"`);
+        let pause = false;
+
+        document.addEventListener('visibilitychange', () => {
+            pause = document.visibilityState !== 'visible';
+        });
+
+        snackbar.addEventListener('mouseenter', () => { pause = true; });
+        snackbar.addEventListener('mouseleave', () => { pause = false; });
+        snackbar.addEventListener('focusin', () => { pause = true; });
+        snackbar.addEventListener('focusout', () => { pause = false; });
+
+        snackbar.querySelector('.ss-close').addEventListener('click', () => {
+            this.hide();
+        });
+
+        if (this.options.autohide) {
+            if (this.timer) {
+                clearInterval(this.timer);
+            }
+
+            this.timer = setInterval(() => {
+                if (!pause) {
+                    this.hide();
+                }
+            }, 5000);
+        }
     }
 
     hide() {
@@ -65,9 +99,6 @@ class simpleSnackbar {
 
         close.classList.add('ss-close');
         close.innerHTML = '<span class="ss-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg></span>';
-        close.addEventListener('click', () => {
-            this.hide();
-        });
 
         snackbar.append(close);
         snackbars.append(snackbar);
@@ -82,6 +113,8 @@ class simpleSnackbar {
         setTimeout(() => {
             snackbar.classList.add('ss-snackbar-active');
         }, 100);
+
+        this.events();
 
         return this;
     }
