76c3978103412d1972cb756f900a5375dfe6f768
Tom Hartley <tom@tomhrtly.com>
2019-10-19 12:35:06 +0100

    Add icon option

--- src/simpleSnackbar.js
+++ src/simpleSnackbar.js
@@ -15,6 +15,7 @@ class simpleSnackbar {
 
         this.defaults = {
             autohide: true,
+            icon: true,
             icons: {
                 success: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>',
                 info: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info-circle" class="svg-inline--fa fa-info-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>',
@@ -96,14 +97,16 @@ class simpleSnackbar {
     }
 
     icon() {
-        if (this.options.type !== 'default') {
-            const snackbar = document.querySelector(`.ss-snackbar[data-id="${this.id}"`);
-            const icon = document.createElement('div');
+        if (this.options.icon) {
+            if (this.options.type !== 'default') {
+                const snackbar = document.querySelector(`.ss-snackbar[data-id="${this.id}"`);
+                const icon = document.createElement('div');
 
-            icon.classList.add('ss-snackbar-icon');
-            icon.innerHTML = `<span class="ss-icon">${this.options.icons[this.options.type]}</span>`;
+                icon.classList.add('ss-snackbar-icon');
+                icon.innerHTML = `<span class="ss-icon">${this.options.icons[this.options.type]}</span>`;
 
-            snackbar.prepend(icon);
+                snackbar.prepend(icon);
+            }
         }
     }
 
