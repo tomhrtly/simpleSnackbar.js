39f1e8d602ebea4976481b0d045ad8009ef625e8
Tom Hartley <tom@tomhrtly.com>
2019-10-19 13:32:22 +0100

    Add close option

--- src/simpleSnackbar.js
+++ src/simpleSnackbar.js
@@ -21,6 +21,7 @@ class simpleSnackbar {
 
         this.defaults = {
             autohide: true,
+            close: true,
             icon: true,
             icons: {
                 success: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>',
@@ -53,6 +54,16 @@ class simpleSnackbar {
         this.init();
     }
 
+    close() {
+        if (this.options.close) {
+            const close = document.createElement('button');
+            close.classList.add('ss-close');
+            close.innerHTML = '<span class="ss-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg></span>';
+
+            this.element.append(close);
+        }
+    }
+
     dispose() {
         this.element.parentNode.removeChild(this.element);
         this.element.dispatchEvent(this.customEvents.disposed);
@@ -70,9 +81,11 @@ class simpleSnackbar {
         this.element.addEventListener('focusin', () => { pause = true; });
         this.element.addEventListener('focusout', () => { pause = false; });
 
-        this.element.querySelector('.ss-close').addEventListener('click', () => {
-            this.hide();
-        });
+        if (this.options.close) {
+            this.element.querySelector('.ss-close').addEventListener('click', () => {
+                this.hide();
+            });
+        }
 
         if (this.options.autohide) {
             if (this.timer) {
@@ -145,7 +158,6 @@ class simpleSnackbar {
     init() {
         const snackbars = document.querySelector('.ss-snackbars');
         const snackbar = document.createElement('div');
-        const close = document.createElement('button');
 
         snackbar.classList.add('ss-snackbar');
         snackbar.setAttribute('tabindex', '0');
@@ -158,14 +170,11 @@ class simpleSnackbar {
         snackbar.innerHTML = `<div class="ss-snackbar-body">${this.message}</div>`;
         this.element = snackbar;
 
-        close.classList.add('ss-close');
-        close.innerHTML = '<span class="ss-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg></span>';
-
-        snackbar.append(close);
         snackbars.prepend(snackbar);
 
-        this.events();
         this.icon();
+        this.close();
+        this.events();
     }
 
     show() {
