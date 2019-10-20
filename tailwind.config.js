7eab8ffad12c72362d3ef68d1faa2c4ca56ddc09
Tom Hartley <tom@tomhrtly.com>
2019-10-20 09:48:40 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            display: ['Quicksand', 'sans-serif'],
+            body: ['Quicksand', 'sans-serif'],
+        },
+        colors: {
+            blue: '#3069c5',
+        },
+    },
+    variants: {},
+    plugins: []
+};
