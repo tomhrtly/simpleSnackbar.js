0d337bc337d5a200303a051f4f1ed8dbcd2f9795
Tom Hartley <tom@tomhrtly.com>
2019-11-06 21:11:19 +0000

    Remove tailwind

---
+++ docs/assets/dist/js/main.js
@@ -0,0 +1,9 @@
+(function (hljs) {
+	'use strict';
+
+	hljs = hljs && hljs.hasOwnProperty('default') ? hljs['default'] : hljs;
+
+	hljs.initHighlightingOnLoad();
+
+}(hljs));
+//# sourceMappingURL=main.js.map
