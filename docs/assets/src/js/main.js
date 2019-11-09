b6db18f9070e7457fead1fc44bf0b512dfd87e3d
Tom Hartley <tom@tomhrtly.com>
2019-11-09 18:32:22 +0000

    Improve demo

--- docs/assets/src/js/main.js
+++ docs/assets/src/js/main.js
@@ -1,5 +1,39 @@
 import SimpleSnackbar from '../../../../src/simpleSnackbar';
 
+let count = 0;
+
+const snackbars = [
+    {
+        message: 'This is a default message!',
+        type: 'default',
+    },
+    {
+        message: 'Form submitted.',
+        type: 'success',
+    },
+    {
+        message: 'Isn\'t this tiny library awesome?',
+        type: 'info',
+    },
+    {
+        message: 'You will need to update your browser soon.',
+        type: 'warning',
+    },
+    {
+        message: 'Please fix the validation errors.',
+        type: 'danger',
+    },
+];
+
 document.querySelector('.demo-js').addEventListener('click', () => {
-    new SimpleSnackbar('This is a message!').show();
+    const snackbar = new SimpleSnackbar(snackbars[count].message, {
+        type: snackbars[count].type,
+    });
+
+    snackbar.show();
+    count += 1;
+
+    if (count === 5) {
+        count = 0;
+    }
 });
